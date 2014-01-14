34db5939319b9340ae87fd48b6c9f6b3d9bc408b
Carlos Moreira <moreira.carlos09@gmail.com>
2014-01-13 20:14:48 -0500

    Reduced if statement in BubbleSort

--- BubbleSort_Algorith.php
+++ BubbleSort_Algorith.php
@@ -2,25 +2,25 @@
 
 $array = [10, 3, 5, 7];
 
-
 //
 function bubble_sort($array, $array_size) {
-    if($array_size == 0) return $array;
-    if ($array == null || sizeof($array) == 0)
-        return 0;
+
+    if ($array == null || sizeof($array) == 0 || $array_size == 0)
+        return $array;
     $holder = 0;
-    for($i=0; $i<$array_size -1 ; $i++){
-        if($array[$i] > $array[$i + 1]){
+    for ($i = 0; $i < $array_size - 1; $i++) {
+        if ($array[$i] > $array[$i + 1]) {
             $holder = $array[$i];
             $array[$i] = $array[$i + 1];
             $array[$i + 1] = $holder;
         }
     }
-   return bubble_sort($array, $array_size-1);
+    return bubble_sort($array, $array_size - 1);
 }
+
 echo "<pre>";
-    print_r($array);
-        $array = bubble_sort($array, sizeof($array));
-    print_r($array);
+print_r($array);
+$array = bubble_sort($array, sizeof($array));
+print_r($array);
 echo "</pre>";
 ?>
